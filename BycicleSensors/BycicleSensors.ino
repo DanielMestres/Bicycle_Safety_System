04590a7ae37c1edf82ea353d2531674de6fa1830
Daniel <daniel.mestres@upr.edu>
2022-11-21 14:13:23 -0400

    implemented distance sensor sampling

--- BycicleSensors/BycicleSensors.ino
+++ BycicleSensors/BycicleSensors.ino
@@ -1,7 +1,9 @@
-// include the library code:
-#include <LiquidCrystal.h>
 #include <avr/io.h>
 #include <util/delay.h>
+
+/*
+* Macros
+*/
 
 #define BIT0 0b00000000
 #define BIT1 0b00000010
@@ -12,87 +14,100 @@
 #define BIT6 0b01000000
 #define BIT7 0b10000000
 
+#define _FUNCTION_SET   0x3B
+#define _DISPLAY_ON     0x0D
+#define _DISPLAY_CLEAR  0x01
+#define _ENTRY_MODE     0x06
+#define _CURSOR_LEFT    0x10
+#define _CURSOR_RIGHT   0x14
+
+/*
+* Globals
+*/
+unsigned long duration;
+unsigned int distanceInInchesSensorA;
+unsigned int distanceInInchesSensorB;
+unsigned int distanceInInchesSensorC;
+
+/*
+* Support Functions
+*/
+
 void setPorts() {
-  DDRC |= BIT2;       // LED OUT
+  // LED OUT
+  DDRL |= BIT6;
+
+  // LCD OUT
+  DDRC |= BIT7;       // RS
+  DDRC |= BIT6;       // E
+  DDRA = 0xff;        // D0 - D7
+
+  // Distance 
+  DDRE |= BIT5;       // Trig OUT
+  DDRE &= ~BIT4;      // Echo IN
 }
 
+void enableLCD() {
+  PORTC |= BIT6;
+  _delay_ms(5);
+  PORTC &= ~BIT6;
+}
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
+void setcmd(int cmd) {
+  PORTC &= ~BIT7;
+  PORTA = cmd;
+  enableLCD();
+  _delay_ms(5);
+}
 
-// LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-// const int trigPin = 3;  // primero es 3
-// const int echoPin = 2;  // primero es 2
+void setdata(int data) {
+  PORTC |= BIT7;
+  PORTA = data;
+  enableLCD();
+  _delay_ms(5);
+}
 
-// const int trigPin2 = 5;  
-// const int echoPin2 = 4;  
-// long duration;
-// int distanceCm, distanceInch, distanceInch2;
+void writeLCD(char* word) {
+    unsigned int index;
+    for(index = 0; index < strlen(word); index++) {
+        setdata(word[index]);
+    }
+}
 
-// int LED = 31;
+void initLCD() {
+  _delay_ms(100);
+  setcmd(_FUNCTION_SET);
+  setcmd(_DISPLAY_ON);
+  setcmd(_DISPLAY_CLEAR);
+  setcmd(_ENTRY_MODE);
+}
 
-// void setup() {
+void sampleDistanceSensors() {
+  PORTE &= ~BIT5;
+  _delay_ms(2);
+  PORTE |= BIT5;
+  _delay_ms(10);
+  PORTE &= ~BIT5;
+  durationA = pulseIn(2, HIGH);    // 2 = E4 Echo In
+  distanceInInchesA = durationA * 0.0133 / 2;
+  if(distanceInInchesA < 6 || distanceInInchesB < 6 || distanceInInchesC < 6) {
+    PORTL |= BIT6;        // Turn ON all the Leds connected to PORTC
+    _delay_ms(100);      // Wait for some time
+    PORTL &= ~BIT6;        // Turn OFF all the Leds connected to PORTC
+    _delay_ms(100);      // Wait for some time
+  }
+}
 
-// //sensor
-//   pinMode(trigPin, OUTPUT);
-//   pinMode(echoPin, INPUT);  
-//    pinMode(trigPin2, OUTPUT);
-//   pinMode(echoPin2, INPUT);  
-//   pinMode(LED, OUTPUT);
-//   // set up the LCD's number of columns and rows:
-//   analogWrite(6, 60);   
-//   lcd.begin(16, 2);
-//   // Print a message to the LCD.
-  
-// }
-
-// void loop() {
-  
-  
-//   digitalWrite(trigPin, LOW);
-//   delayMicroseconds(2);
-//   digitalWrite(trigPin, HIGH);
-//   delayMicroseconds(10);
-//   digitalWrite(trigPin, LOW);
-//   duration = pulseIn(echoPin, HIGH);
-//   distanceCm = duration * 0.034 / 2;
-//   distanceInch = duration * 0.0133 / 2;
-
-//   digitalWrite(trigPin2, LOW);
-//   delayMicroseconds(2);
-//   digitalWrite(trigPin2, HIGH);
-//   delayMicroseconds(10);
-//   digitalWrite(trigPin2, LOW);
-//   duration = pulseIn(echoPin2, HIGH);
-//   distanceInch2 = duration * 0.0133 / 2;
-
-// if(distanceInch < 6  || distanceInch2 < 6){
-//   digitalWrite(LED, HIGH);
-// }
-// else{
-//   digitalWrite(LED, LOW);
-// }
-
-
-//   lcd.setCursor(0, 0); // Sets the location at which subsequent text written to the LCD will be displayed
-//   lcd.print("Distance: "); // Prints string "Distance" on the LCD
-//   lcd.print(distanceCm); // Prints the distance value from the sensor
-//   lcd.print(" cm");
-//   delay(10);
-//   lcd.setCursor(0, 1);
-//   lcd.print("Distance: ");
-//   lcd.print(distanceInch);
-//   lcd.print(" inch");
-//   delay(10);
-// }
+/*
+* Main
+*/
 
 void setup() {
   setPorts();
+  initLCD();
+  writeLCD("Hello World!");
 }
 
 void loop() {
-  PORTC |= BIT2;        // Turn ON all the Leds connected to PORTC
-  _delay_ms(100);      // Wait for some time
-  PORTC &= ~BIT2;        // Turn OFF all the Leds connected to PORTC
-  _delay_ms(100);      // Wait for some time
+  sampleDistanceSensor();
 }
